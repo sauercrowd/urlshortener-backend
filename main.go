19705fc221777c754e2f1b40d125698054d8a21f
sauercrowd <jonadev95@posteo.org>
2017-12-22 09:30:34 +0100

    added zip

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"log"
+
 	"github.com/sauercrowd/urlshortener-backend/pkg/flags"
+	"github.com/sauercrowd/urlshortener-backend/pkg/persistence"
+	"github.com/sauercrowd/urlshortener-backend/pkg/web"
 )
+
+const basename = "http://example.org"
 
 func main() {
 	f := flags.Parse()
+	storage, err := persistence.Create(f)
+	if err != nil {
+		log.Fatal("Could not connect to redis:", err)
+	}
+	ctx := web.Context{
+		Redis:    storage,
+		Basename: basename,
+	}
+	web.Handle(":8080", &ctx)
 }
